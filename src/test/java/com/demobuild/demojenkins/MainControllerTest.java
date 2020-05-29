9e586698efe2b5e9d00f1c6ebe3026af0bbdbe07
anthonyvalle <valleganthony@gmail.com>
2020-05-29 13:06:50 -0400

    Test class updates

--- src/test/java/com/demobuild/demojenkins/MainControllerTest.java
+++ src/test/java/com/demobuild/demojenkins/MainControllerTest.java
@@ -2,19 +2,28 @@ package com.demobuild.demojenkins;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+
+@WebMvcTest(MainController.class)
 class MainControllerTest {
 
     @Autowired
-    private MainController controller;
+    private MockMvc mockMvc;
 
     @Test
-    void sayHelloWorld() {
-        assertEquals("Hello World", controller.sayHelloWorld());
+    void sayHelloWorld() throws Exception {
+
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+
     }
 }
